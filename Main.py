c42dacde903d9e3d72835aab5186ec36b38dc5bc
Jorge Mata <juniormata@verizon.net>
2019-11-18 15:05:14 -0800

    first build

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 
+# mainWindow configurations
 mainWindow = tk.Tk()    # initialized window in mainWindow variable
 mainWindow.title("Speed Wheel")     # named the program Speed Wheel 
 mainWindow.configure(background="#121212")      # made mainWindow have a black background
-
 
 
 # mainMenu configurations
@@ -15,5 +15,16 @@ optionMenu.add_command(label="Option 1")        # adds a menu button called Opti
 optionMenu.add_command(label="Option 2")        # adds a menu button called Option 2 (this can be changed later)
 optionMenu.add_separator()      # this adds a line separator to the option menu
 optionMenu.add_command(label='Exit', command=mainWindow.destroy)        # This adds a button in the menu that exits the program
+
+
+# Frame configurations
+buttonFrame = tk.Frame (mainWindow)     # this adds a frame that contains the liquid and break type option buttons
+buttonFrame.pack()      # This unpacked the buttonFrame
+
+wheelFrame = tk.Frame (mainWindow)
+wheelFrame.pack()
+
+syringeFrame = tk.Frame (mainWindow)        #this is the frame that will contain the syringe model
+syringeFrame.pack(anchor=tk.E)     # This unpacks the syringeFrame
 
 mainWindow.mainloop()       # set the mainWindow on the loop
