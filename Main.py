f74655984a0281cae7d5457c0bd6d49db49611fa
Jorge Mata <juniormata@verizon.net>
2019-11-18 14:34:47 -0800

    Added a menu bar in the program

--- Main.py
+++ Main.py
@@ -1,7 +1,19 @@
 import tkinter as tk
 
-mainWindow = tk.Tk()
-mainWindow.title("Speed Wheel")
-mainWindow.configure(background="#121212")
+mainWindow = tk.Tk()    # initialized window in mainWindow variable
+mainWindow.title("Speed Wheel")     # named the program Speed Wheel 
+mainWindow.configure(background="#121212")      # made mainWindow have a black background
 
-mainWindow.mainloop()
+
+
+# mainMenu configurations
+mainMenu = tk.Menu (mainWindow)     # set a new menu tab named mainMenu to mainWindow
+mainWindow.config(menu=mainMenu)    # added the mainMenu to the mainWindow
+optionMenu = tk.Menu (mainMenu)     # added another a menu in the mainMenu stored in optionMenu (This works a lot like frames)
+mainMenu.add_cascade(label="Options", menu=optionMenu)      # adds button group called Options in mainMenu
+optionMenu.add_command(label="Option 1")        # adds a menu button called Option 1 (this can be changed later)
+optionMenu.add_command(label="Option 2")        # adds a menu button called Option 2 (this can be changed later)
+optionMenu.add_separator()      # this adds a line separator to the option menu
+optionMenu.add_command(label='Exit', command=mainWindow.destroy)        # This adds a button in the menu that exits the program
+
+mainWindow.mainloop()       # set the mainWindow on the loop
