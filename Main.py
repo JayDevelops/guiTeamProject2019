6737fa6502c05d0c3b36632251c03cabb40c1a42
JayDevelops <jaymations1234@gmail.com>
2019-11-18 15:30:00 -0800

    jp branch here

--- Main.py
+++ Main.py
@@ -1,15 +1,17 @@
 import tkinter as tk
 
+# JP BRANCH HERE
+
 # mainWindow configurations
 mainWindow = tk.Tk()    # initialized window in mainWindow variable
-mainWindow.title("Speed Wheel")     # named the program Speed Wheel 
+mainWindow.title("Speed Wheel")     # named the program Speed Wheel
 mainWindow.configure(background="#121212")      # made mainWindow have a black background
 
 
 # mainMenu configurations
-mainMenu = tk.Menu (mainWindow)     # set a new menu tab named mainMenu to mainWindow
+mainMenu = tk.Menu(mainWindow)     # set a new menu tab named mainMenu to mainWindow
 mainWindow.config(menu=mainMenu)    # added the mainMenu to the mainWindow
-optionMenu = tk.Menu (mainMenu)     # added another a menu in the mainMenu stored in optionMenu (This works a lot like frames)
+optionMenu = tk.Menu(mainMenu)     # added another a menu in the mainMenu stored in optionMenu (This works a lot like frames)
 mainMenu.add_cascade(label="Options", menu=optionMenu)      # adds button group called Options in mainMenu
 optionMenu.add_command(label="Option 1")        # adds a menu button called Option 1 (this can be changed later)
 optionMenu.add_command(label="Option 2")        # adds a menu button called Option 2 (this can be changed later)
@@ -18,13 +20,13 @@ optionMenu.add_command(label='Exit', command=mainWindow.destroy)        # This a
 
 
 # Frame configurations
-buttonFrame = tk.Frame (mainWindow)     # this adds a frame that contains the liquid and break type option buttons
+buttonFrame = tk.Frame(mainWindow)     # this adds a frame that contains the liquid and break type option buttons
 buttonFrame.pack()      # This unpacked the buttonFrame
 
-wheelButtonFrame = tk.Frame (mainWindow)
+wheelButtonFrame = tk.Frame(mainWindow)
 wheelButtonFrame.pack()
 
-syringeFrame = tk.Frame (mainWindow)        #this is the frame that will contain the syringe model
-syringeFrame.pack(anchor=tk.E)     # This unpacks the syringeFrame
+syringeFrame = tk.Frame(mainWindow)  # This is the frame that will contain the syringe model
+syringeFrame.pack(anchor=tk.E)  # This unpacks the syringeFrame
 
-mainWindow.mainloop()       # set the mainWindow on the loop
+mainWindow.mainloop()  # set the mainWindow on the loop
