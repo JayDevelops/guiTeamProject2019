410cf62ffda4aa74c5a343597bdf0ac8e1f2de0e
JayDevelops <jaymations1234@gmail.com>
2019-12-05 20:19:58 -0800

    added subclasses of syrup, water, etc from inheritance of liquid class

--- Liquid.py
+++ Liquid.py
@@ -4,7 +4,7 @@ import Syringe
 class liquid:
     def __init__(self, canvas, color, pump):
         self.y = 0
-        
+
         self.canvas = canvas
         self.color = color
         self.pump = pump
@@ -31,4 +31,103 @@ class liquid:
                 self.canvas.after_cancel(liquidMove)
         else:
             pass
-        
+
+
+# BELOW ARE THE LIQUIDS WHICH TRANSCEND TO THE SLOWEST SPEED
+
+# This is an oil subclass which inherits, methods and init, everything from liquid
+class oilLiquid(liquid):
+    def move(self):
+        # This oil liquid will move faster than liquid
+        if(not(self.pump.getHead() == self.pump.getLimit())):
+            liquidMove = self.canvas.after(10, self.move)
+            x0, y0, x1, y1 = self.canvas.coords(self.model)
+            y1 += 8
+            if(self.pump.getHead() == self.pump.getLimit()):
+                self.canvas.after_cancel(liquidMove)
+        else:
+            pass
+
+    def reset(self):
+        if(not(self.pump.getHead() == self.pump.getLimit2())):
+            liquidMove = self.canvas.after(10, self.reset)
+            x0, y0, x1, y1 = self.canvas.coords(self.model)
+            y1 -= 8
+            self.canvas.coords(self.model, x0, y0, x1, y1)
+            if(self.pump.getHead() == self.pump.getLimit2()):
+                self.canvas.after_cancel(liquidMove)
+        else:
+            pass
+
+# This is a water subclass which inherits, methods and init, everything from liquid
+class waterLiquid(liquid):
+    def move(self):
+        # This oil liquid will move slower
+        if(not(self.pump.getHead() == self.pump.getLimit())):
+            liquidMove = self.canvas.after(10, self.move)
+            x0, y0, x1, y1 = self.canvas.coords(self.model)
+            y1 += 7
+            if(self.pump.getHead() == self.pump.getLimit()):
+                self.canvas.after_cancel(liquidMove)
+        else:
+            pass
+
+    def reset(self):
+        if(not(self.pump.getHead() == self.pump.getLimit2())):
+            liquidMove = self.canvas.after(10, self.reset)
+            x0, y0, x1, y1 = self.canvas.coords(self.model)
+            y1 -= 7
+            self.canvas.coords(self.model, x0, y0, x1, y1)
+            if(self.pump.getHead() == self.pump.getLimit2()):
+                self.canvas.after_cancel(liquidMove)
+        else:
+            pass
+
+# This is a water subclass which inherits, methods and init, everything from liquid
+class alcoholLiquid(liquid):
+    def move(self):
+        # This oil liquid will move slower
+        if(not(self.pump.getHead() == self.pump.getLimit())):
+            liquidMove = self.canvas.after(10, self.move)
+            x0, y0, x1, y1 = self.canvas.coords(self.model)
+            y1 += 6
+            if(self.pump.getHead() == self.pump.getLimit()):
+                self.canvas.after_cancel(liquidMove)
+        else:
+            pass
+
+    def reset(self):
+        if(not(self.pump.getHead() == self.pump.getLimit2())):
+            liquidMove = self.canvas.after(10, self.reset)
+            x0, y0, x1, y1 = self.canvas.coords(self.model)
+            y1 -= 6
+            self.canvas.coords(self.model, x0, y0, x1, y1)
+            if(self.pump.getHead() == self.pump.getLimit2()):
+                self.canvas.after_cancel(liquidMove)
+        else:
+            pass
+
+
+# This is a syrup subclass which inherits everything, methods and init, from liquid
+class syrupColor(liquid):
+    def move(self):
+        # This oil liquid will move slower
+        if(not(self.pump.getHead() == self.pump.getLimit())):
+            liquidMove = self.canvas.after(10, self.move)
+            x0, y0, x1, y1 = self.canvas.coords(self.model)
+            y1 += 3
+            if(self.pump.getHead() == self.pump.getLimit()):
+                self.canvas.after_cancel(liquidMove)
+        else:
+            pass
+
+    def reset(self):
+        if(not(self.pump.getHead() == self.pump.getLimit2())):
+            liquidMove = self.canvas.after(10, self.reset)
+            x0, y0, x1, y1 = self.canvas.coords(self.model)
+            y1 -= 3
+            self.canvas.coords(self.model, x0, y0, x1, y1)
+            if(self.pump.getHead() == self.pump.getLimit2()):
+                self.canvas.after_cancel(liquidMove)
+        else:
+            pass
--- Main.py
+++ Main.py
@@ -24,62 +24,62 @@ def switchL():
 
 # mainWindow configurations
 mainWindow = tk.Tk()    # initialized window in mainWindow variable
-mainWindow.title("Speed Wheel")     # named the program Speed Wheel 
+mainWindow.title("Speed Wheel")     # named the program Speed Wheel
 mainWindow.configure(background="#121212")      # made mainWindow have a black background
-mainWindow.geometry("1280x720") # This sets the window to be 720p
-mainWindow.resizable(0, 0) # This makes the window not resizable 
+mainWindow.geometry("1280x720")  # This sets the window to be 720p
+mainWindow.resizable(0, 0)  # This makes the window not resizable
 
 # mainMenu configurations
-mainMenu = tk.Menu (mainWindow)     # set a new menu tab named mainMenu to mainWindow
-mainWindow.config(menu=mainMenu)    # added the mainMenu to the mainWindow
-optionMenu = tk.Menu (mainMenu)     # added another a menu in the mainMenu stored in optionMenu (This works a lot like frames)
-mainMenu.add_cascade(label="Options", menu=optionMenu)      # adds button group called Options in mainMenu
-optionMenu.add_command(label="Option 1")        # adds a menu button called Option 1 (this can be changed later)
-optionMenu.add_command(label="Option 2")        # adds a menu button called Option 2 (this can be changed later)
-optionMenu.add_separator()      # this adds a line separator to the option menu
-optionMenu.add_command(label='Exit', command=mainWindow.destroy)        # This adds a button in the menu that exits the program
+mainMenu = tk.Menu(mainWindow)  # set a new menu tab named mainMenu to mainWindow
+mainWindow.config(menu=mainMenu) # added the mainMenu to the mainWindow
+optionMenu = tk.Menu(mainMenu)  # added another a menu in the mainMenu stored in optionMenu (This works a lot like frames)
+mainMenu.add_cascade(label="Options", menu=optionMenu)   # adds button group called Options in mainMenu
+optionMenu.add_command(label="Option 1")   # adds a menu button called Option 1 (this can be changed later)
+optionMenu.add_command(label="Option 2")   # adds a menu button called Option 2 (this can be changed later)
+optionMenu.add_separator()    # This adds a line separator to the option menu
+optionMenu.add_command(label='Exit', command=mainWindow.destroy)  # This adds a button in the menu that exits the program
 
 
 # Frame configurations
 
-syringeFrame = tk.Frame (mainWindow)    # This frame contains the syringe model
+syringeFrame = tk.Frame(mainWindow)    # This frame contains the syringe model
 syringeFrame.pack(side=tk.RIGHT)    # This unpacks the main syringe frame
 
 interactFrame = tk.Frame(mainWindow)    # This frame is a parent frame and contains the buttons and the wheel model
 interactFrame.pack(side=tk.RIGHT)   # This unpacks the interact parent frame
 
-buttonFrame = tk.Frame (interactFrame)  # This frame is a child frame and it contains the buttons
+buttonFrame = tk.Frame(interactFrame)  # This frame is a child frame and it contains the buttons
 buttonFrame.pack()  # This unpacks the button frame
 
-brakeButtonFrame = tk.Frame (buttonFrame)   # This frame contains the buttons for the brake feature
-brakeButtonFrame.grid(row=0, column=0)  # This unpacks the brake button frame 
+brakeButtonFrame = tk.Frame(buttonFrame)   # This frame contains the buttons for the brake feature
+brakeButtonFrame.grid(row=0, column=0)  # This unpacks the brake button frame
 
-liquidButtonFrame = tk.Frame (buttonFrame)  # This frame contains the buttons for the iquids
-liquidButtonFrame.grid(row=0, column=0) # This unpacks the liquid button frame
+liquidButtonFrame = tk.Frame(buttonFrame)  # This frame contains the buttons for the iquids
+liquidButtonFrame.grid(row=0, column=0)  # This unpacks the liquid button frame
 
 
-wheelFrame = tk.Frame (interactFrame)   # This frame contains the wheel model
+wheelFrame = tk.Frame(interactFrame)   # This frame contains the wheel model
 wheelFrame.pack()   # This unpacks the wheel model frame
 
 syringeCanvas = tk.Canvas(syringeFrame, height=700, width=400, bd=0, bg="#121212")     # This creates a new canvas that contains the syringe model
 syringeCanvas.pack()        # This unpacks the syringe model
 
-wheelCanvas = tk.Canvas (wheelFrame, height=650, width=900, bg="#121212")
+wheelCanvas = tk.Canvas(wheelFrame, height=650, width=900, bg="#121212")
 wheelCanvas.pack()
 
 
 # This is the limit that the pump will go up to but not pass
-backLimit = syringeCanvas.create_rectangle(10, 365, 150, 385, width=1, outline= "#121212", fill="#121212")
-frontLimit = syringeCanvas.create_rectangle(10, 10, 150, 30, width=1, outline= "#121212", fill="#121212")
+backLimit = syringeCanvas.create_rectangle(10, 365, 150, 385, width=1, outline="#121212", fill="#121212")
+frontLimit = syringeCanvas.create_rectangle(10, 10, 150, 30, width=1, outline="#121212", fill="#121212")
 
 # Class properties
 
-pump = Syringe.pump(syringeCanvas, backLimit, frontLimit)   # This creates a pump object and stores it into pump 
+pump = Syringe.pump(syringeCanvas, backLimit, frontLimit)  # This creates a pump object and stores it into pump
 
 
 liquid = Liquid.liquid(syringeCanvas, waterColor, pump)
 
-syringe = Syringe.syringe(syringeCanvas)      # This calls the syringe class through the Syringe.py file and creates the syringe model in the syringeCanvas
+syringe = Syringe.syringe(syringeCanvas)  # This calls the syringe class through the Syringe.py file and creates the syringe model in the syringeCanvas
 
 
 # user defined functions for the tkinter buttons
@@ -96,50 +96,49 @@ def reset():
 
 
 # Liquid Button Properties
-pumpResetButtonL = tk.Button (liquidButtonFrame, width=10, text="Reset", command=reset)
+pumpResetButtonL = tk.Button(liquidButtonFrame, width=10, text="Reset", command=reset)
 pumpResetButtonL.pack(side=tk.LEFT)
 
-pumpButtonL = tk.Button (liquidButtonFrame, width=10, text="Pump", command=pumpMove)
+pumpButtonL = tk.Button(liquidButtonFrame, width=10, text="Pump", command=pumpMove)
 pumpButtonL.pack(side=tk.LEFT)
 
-syrupButton = tk.Button (liquidButtonFrame, width=10, text="Syrup")
+syrupButton = tk.Button(liquidButtonFrame, width=10, text="Syrup")
 syrupButton.pack(side=tk.LEFT)
 
-oilButton = tk.Button (liquidButtonFrame, width=10, text="Motor Oil")
+oilButton = tk.Button(liquidButtonFrame, width=10, text="Motor Oil")
 oilButton.pack(side=tk.LEFT)
 
-waterButton = tk.Button (liquidButtonFrame, width=10, text="Water")
+waterButton = tk.Button(liquidButtonFrame, width=10, text="Water")
 waterButton.pack(side=tk.LEFT)
 
-alcoholButton = tk.Button (liquidButtonFrame, width=10, text="Alcohol")
+alcoholButton = tk.Button(liquidButtonFrame, width=10, text="Alcohol")
 alcoholButton.pack(side=tk.LEFT)
 
-switchButtonL = tk.Button (liquidButtonFrame, width=10, text="Switch", command=switchB)
+switchButtonL = tk.Button(liquidButtonFrame, width=10, text="Switch", command=switchB)
 switchButtonL.pack(side=tk.LEFT)
 
 
 # Brake Button Properties
-pumpResetButtonB = tk.Button (brakeButtonFrame, width=10, text="Reset", command=reset)
+pumpResetButtonB = tk.Button(brakeButtonFrame, width=10, text="Reset", command=reset)
 pumpResetButtonB.pack(side=tk.LEFT)
 
-pumpButtonB = tk.Button (brakeButtonFrame, width=10, text="Pump", command=pumpMove)
+pumpButtonB = tk.Button(brakeButtonFrame, width=10, text="Pump", command=pumpMove)
 pumpButtonB.pack(side=tk.LEFT)
 
-sandpaperButton = tk.Button (brakeButtonFrame, width=10, text="Sand Paper")
+sandpaperButton = tk.Button(brakeButtonFrame, width=10, text="Sand Paper")
 sandpaperButton.pack(side=tk.LEFT)
 
-towelButton = tk.Button (brakeButtonFrame, width=10, text="Towel")
+towelButton = tk.Button(brakeButtonFrame, width=10, text="Towel")
 towelButton.pack(side=tk.LEFT)
 
-spongeButton = tk.Button (brakeButtonFrame, width=10, text="Sponge")
+spongeButton = tk.Button(brakeButtonFrame, width=10, text="Sponge")
 spongeButton.pack(side=tk.LEFT)
 
-foilButton = tk.Button (brakeButtonFrame, width=10, text="Aluminum Foil")
+foilButton = tk.Button(brakeButtonFrame, width=10, text="Aluminum Foil")
 foilButton.pack(side=tk.LEFT)
 
-switchButtonB = tk.Button (brakeButtonFrame, width=10, text="Switch", command=switchL)
+switchButtonB = tk.Button(brakeButtonFrame, width=10, text="Switch", command=switchL)
 switchButtonB.pack(side=tk.LEFT)
 
 
-
-mainWindow.mainloop()       # set the mainWindow on the loop
+mainWindow.mainloop()  # Set the mainWindow on the loop
