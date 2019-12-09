cdcb7668415dcc16295173874c1d51d200e7b70d
Jorge Mata <juniormata@verizon.net>
2019-12-08 22:57:44 -0800

    made setLiquidColor and setLiquidSpeed method

--- Liquid.py
+++ Liquid.py
@@ -2,19 +2,19 @@ import tkinter as tk
 import Syringe
 
 class liquid:
-    def __init__(self, canvas, color, pump):
+    def __init__(self, canvas, pump):
         self.y = 0
-        
+        self.liquidSpeed = 2
+
         self.canvas = canvas
-        self.color = color
         self.pump = pump
-        self.model = canvas.create_rectangle(10, 10, 150, 10, fill=self.color, width=0)
+        self.model = canvas.create_rectangle(10, 10, 150, 10, fill="#ffffff", width=0)
 
     def move(self):
         if(not(self.pump.getHead() == self.pump.getLimit())):
             liquidMove = self.canvas.after(10, self.move)
             x0, y0, x1, y1 = self.canvas.coords(self.model)
-            y1 += 2
+            y1 += self.liquidSpeed
             self.canvas.coords(self.model, x0, y0, x1, y1)
             if(self.pump.getHead() == self.pump.getLimit()):
                 self.canvas.after_cancel(liquidMove)
@@ -25,10 +25,15 @@ class liquid:
         if(not(self.pump.getHead() == self.pump.getLimit2())):
             liquidMove = self.canvas.after(10, self.reset)
             x0, y0, x1, y1 = self.canvas.coords(self.model)
-            y1 -= 2
+            y1 -= self.liquidSpeed
             self.canvas.coords(self.model, x0, y0, x1, y1)
             if(self.pump.getHead() == self.pump.getLimit2()):
                 self.canvas.after_cancel(liquidMove)
         else:
             pass
         
+    def setLiquidSpeed(self, speed):
+        self.liquidSpeed = speed
+
+    def setLiquidColor(self, color):
+        self.canvas.itemconfig(self.model, fill= color)
